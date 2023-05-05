036d6c105d9d36a7d869a07a67fd8d57d96500ba
JEP\kaito <kaito.jep@gmail.com>
2023-05-05 21:49:57 +0800

    Created fe ui ( title, click counter and button )

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
 function App() {
+
+    const handleBtnClick = async (e) => {
+    }
+
     return (
         <div className="container">
-            <div className="row justify-content-center">
-                <div className="col-md-8">
-                    <div className="card">
-                        <div className="card-header">App Component</div>
-
-                        <div className="card-body">I'm an example component!</div>
+            <div className="row">
+                <div className="col">
+                    <div className="d-block text-center p-3">
+                        <h1 className="fw-bold mb-3">ClickMe</h1>
+                        <div className="h3 mb-4 mb-lg-5 total-clicks">
+                            Total Clicks Today: <div className="cntr fw-bold">0</div>
+                        </div>
+                        <div className="mt-2">
+                            <button onClick={handleBtnClick} type="button" className="btn btn-primary">Click Me!</button>
+                        </div>
                     </div>
                 </div>
             </div>
