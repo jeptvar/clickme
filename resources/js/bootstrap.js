24108607cf55fac3e37601a75f6f792e0741cbdc
JEP\kaito <kaito.jep@gmail.com>
2023-05-05 21:45:15 +0800

    Setup blade and react ui

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,15 +1,33 @@
 window._ = require('lodash');
 
 /**
+ * We'll load jQuery and the Bootstrap jQuery plugin which provides support
+ * for JavaScript based Bootstrap features such as modals and tabs. This
+ * code may be modified to fit the specific needs of your application.
+ */
+
+try {
+    window.Popper = require('popper.js').default;
+    window.$ = window.jQuery = require('jquery');
+
+    require('bootstrap');
+} catch (e) {}
+
+/**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
 window.axios = require('axios');
-
+window.axios.defaults.baseURL = document.head.querySelector('meta[name="domain"]').content;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
+/**
+ * API Window variable
+ */
+const domain = document.head.querySelector('meta[name="domain"]').content;
+window.api = `${domain}/api`;
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
@@ -24,5 +42,5 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //     broadcaster: 'pusher',
 //     key: process.env.MIX_PUSHER_APP_KEY,
 //     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-//     forceTLS: true
+//     encrypted: true
 // });
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::view('/{path?}', 'app')
+    ->where('path', '.*')
+    ->name('react');
