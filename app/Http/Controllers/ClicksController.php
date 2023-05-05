fcdf1225ad8ee28241a0cf46cd0b86b51f8825c8
JEP\kaito <kaito.jep@gmail.com>
2023-05-05 22:09:12 +0800

    Added fe functionality - connected to api

--- app/Http/Controllers/ClicksController.php
+++ app/Http/Controllers/ClicksController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Click;
 
 class ClicksController extends Controller
 {
--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -3,8 +3,36 @@ import ReactDOM from 'react-dom';
 
 function App() {
 
-    const handleBtnClick = async (e) => {
+    const [clicks, setClicks] = useState({});
+    const date = new Date().toLocaleDateString('en-CA');
+
+    const getClicks = async () => {
+        const resp = await fetch(`${window.api}/clicks/fetch?date=${date}`)
+        const data = await resp.json();
+
+        setClicks(data);
     }
+
+    useEffect(() => {
+        getClicks();
+    }, []);
+
+    const handleBtnClick = async (e) => {
+        const resp = await fetch(`${window.api}/clicks/trigger`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({date})
+        });
+        const data = await resp.json();
+
+        setClicks(data);
+    }
+
+    useEffect(() => {
+        console.log(clicks);
+    }, [clicks]);
 
     return (
         <div className="container">
@@ -13,7 +41,7 @@ function App() {
                     <div className="d-block text-center p-3">
                         <h1 className="fw-bold mb-3">ClickMe</h1>
                         <div className="h3 mb-4 mb-lg-5 total-clicks">
-                            Total Clicks Today: <div className="cntr fw-bold">0</div>
+                            Total Clicks Today: <div className="cntr fw-bold">{Object.keys(clicks).length ? clicks.clicks_count : 0}</div>
                         </div>
                         <div className="mt-2">
                             <button onClick={handleBtnClick} type="button" className="btn btn-primary">Click Me!</button>
